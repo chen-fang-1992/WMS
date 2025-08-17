908f2d6e4663ee02ed3f13d37f09c7c4bb92dd80
Chen Fang <chenfangau@gmail.com>
2025-08-17 12:02:17 +1000

    路由问题

--- tms/views.py
+++ tms/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import render, redirect
+from django.urls import get_resolver, Resolver404
 
 def page_not_found(request, exception=None):
+	if (request.method in ('GET','HEAD')
+		and getattr(settings, 'APPEND_SLASH', True)
+		and not request.path.endswith('/')):
+		try:
+			match = get_resolver().resolve(request.path + '/')
+			return redirect(request.path + '/')
+		except Resolver404:
+			pass
 	return render(request, 'errors/404.html', status=404)
